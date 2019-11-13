4b93e43120f091c37ecb2f34eb56aa9ada5bc53c
José Yoshiriro <jose.yoshihiro@bandtec.com.br>
2019-11-13 11:31:20 -0300

    add atributo "field"

--- projeto-site/models/usuario.js
+++ projeto-site/models/usuario.js
@@ -8,19 +8,23 @@ https://codewithhugo.com/sequelize-data-types-a-practical-guide/
 module.exports = (sequelize, DataTypes) => {
     let Usuario = sequelize.define('Usuario',{
 		id: {
+			field: 'id',
 			type: DataTypes.INTEGER,
 			primaryKey: true,
 			autoIncrement: true
 		},		
 		nome: {
+			field: 'nome',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
 		login: {
+			field: 'login',
 			type: DataTypes.STRING,
 			allowNull: false
 		},
 		senha: {
+			field: 'senha',
 			type: DataTypes.STRING,
 			allowNull: false
 		}
